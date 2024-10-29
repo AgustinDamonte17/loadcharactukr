f9775c5800af0d8c538acd9483d694a8c9bf3b6c
Agustin <agusda95o@gmail.com>
2024-10-29 18:17:35 -0300

    rama funcional para el output de los 238 profiles

--- elim_v_interm.py
+++ elim_v_interm.py
@@ -4,7 +4,7 @@ def elim_v_interm():
     carpeta = r'C:\Users\adamonte\Desktop\AGUSTIN\Python\LoadCharacterizationUkraine\CsvPorCostid'
     prefijo = 'inputpromedidores'
     
-    # Verificar si la carpeta existe antes de intentar acceder
+    # Verificar si la carpeta existe antes de intentar acceder prueba cambio
     if not os.path.exists(carpeta):
         print(f"La carpeta especificada no existe: {carpeta}")
         return
